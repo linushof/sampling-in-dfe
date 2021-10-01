9de40ce6d983154d26ee5792484158b600571233
linushof <linus.hof@stud.uni-heidelberg.de>
2021-10-01 16:55:41 +0200

    Delete old files

--- R/fun_inits_MCMC_pooled.R
+++
@@ -1,4 +0,0 @@
-inits_MCMC_pooled <- function(){list("alpha" = 1,
-                                     "gamma.pre" = .5,
-                                     "delta.pre" = .5,
-                                     "rho.pre" = 0)}
