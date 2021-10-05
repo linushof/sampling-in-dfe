e1ae74661f39949442ebfadbc4c7ea8b70c5ef09
linushof <linus.hof@stud.uni-heidelberg.de>
2021-10-05 15:58:22 +0200

    Delete files of hierarchical estimation

--- R/fun_inits_MCMC_hier.R
+++
@@ -1,4 +0,0 @@
-inits_MCMC <- function(){list("mu.alpha.prob" = 1,
-                              "mu.gamma.prob" = 2,
-                              "mu.delta.prob" = 2,
-                              "mu.rho.log" = -0.5)}
